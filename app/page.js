458135dc480ef112855df213a4b05541059517c4
David Fischer <fiscd@seas.upenn.edu>
2025-01-27 20:32:57 -0500

    commit

--- app/page.js
+++ app/page.js
@@ -38,56 +38,50 @@ export default function Home() {
   };
 
   return (
-    <Box sx={{ p: 4, textAlign: "center", mt: 12 }}>
+    <Box sx={{ p: 4, textAlign: "center", mt: 12, position: "relative" }}>
       <Typography variant="h4" gutterBottom sx={{ textWrap: "balance" }}>
         Select subsets of the data to be used for training and testing the model
       </Typography>
 
-      <Grid container spacing={4} justifyContent="center" alignItems="center">
-        {/* Training Section */}
-        <Grid xs={12} md={3} className="training-container">
-          <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <Typography mb={1} variant="h6" sx={{ textWrap: "balance" }}>Training</Typography>
-            <Box className="tube">
-              <Box className="tube-fill training-fill" sx={{ height: calculateFill("training") }} />
-            </Box>
+      <Box sx={{ position: "relative", display: "flex", justifyContent: "center", alignItems: "center", height: "auto" }}>
+        {/* Training Section (Far Left) */}
+        <Box className="training-container">
+          <Typography mb={1} variant="h6" sx={{ textWrap: "balance" }}>Training</Typography>
+          <Box className="tube">
+            <Box className="tube-fill training-fill" sx={{ height: calculateFill("training") }} />
           </Box>
-        </Grid>
+        </Box>
 
-        {/* Dataset Selection */}
-        <Grid xs={12} md={4} className="dataset-section">
-          <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt: 6 }}>
-            <Typography variant="h6" mb={2} sx={{ textWrap: "balance" }}>Data Subsets</Typography>
-            {Object.keys(datasets).map((dataset, index) => (
-              <Box key={dataset} className="subset-container">
-                <Checkbox checked={datasets[dataset].training} onChange={() => handleChange(dataset, "training")} />
-                <Box className="subset-box">{index + 1}</Box>
-                <Checkbox checked={datasets[dataset].testing} onChange={() => handleChange(dataset, "testing")} />
-              </Box>
-            ))}
-            
-            {/* Run Model Button */}
-            <Button 
-              variant="contained" 
-              className="run-model-button"
-              onClick={handleRunModel}
-              disabled={isButtonDisabled()}
-            >
-              Run Model
-            </Button>
-          </Box>
-        </Grid>
-
-        {/* Testing Section */}
-        <Grid xs={12} md={3} className="testing-container">
-          <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <Typography mb={1} variant="h6" sx={{ textWrap: "balance" }}>Testing</Typography>
-            <Box className="tube">
-              <Box className="tube-fill testing-fill" sx={{ height: calculateFill("testing") }} />
+        {/* Dataset Selection (Center) */}
+        <Box className="dataset-section">
+          <Typography variant="h6" mb={2} sx={{ textWrap: "balance" }}>Data Subsets</Typography>
+          {Object.keys(datasets).map((dataset, index) => (
+            <Box key={dataset} className="subset-container">
+              <Checkbox checked={datasets[dataset].training} onChange={() => handleChange(dataset, "training")} />
+              <Box className="subset-box">{index + 1}</Box>
+              <Checkbox checked={datasets[dataset].testing} onChange={() => handleChange(dataset, "testing")} />
             </Box>
+          ))}
+          
+          {/* Run Model Button */}
+          <Button 
+            variant="contained" 
+            className="run-model-button"
+            onClick={handleRunModel}
+            disabled={isButtonDisabled()}
+          >
+            Run Model
+          </Button>
+        </Box>
+
+        {/* Testing Section (Far Right) */}
+        <Box className="testing-container">
+          <Typography mb={1} variant="h6" sx={{ textWrap: "balance" }}>Testing</Typography>
+          <Box className="tube">
+            <Box className="tube-fill testing-fill" sx={{ height: calculateFill("testing") }} />
           </Box>
-        </Grid>
-      </Grid>
+        </Box>
+      </Box>
     </Box>
   );
 }
